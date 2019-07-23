9ad307848833197b63c3f7c09b0fdbc42398a238
Victor Gianvechio <victor.gianvecchio@live.com>
2019-07-23 10:47:10 -0300

    new functions and v1.0.1

--- test/test.js
+++ test/test.js
@@ -2,20 +2,7 @@ const cmd = require('../lib/win-cmd.js')
 const path = require('path')
 
 try {
-    console.log('\nlistFolder:', cmd.listFiles('./my-folder/'))
+    console.log('\nlistFolder:', cmd.setSystemEnv('PATH', 'teste\\victor\\bin\\NOVO'))
 } catch (err) {
     console.log(err.message)
 }
-
-/*
-checkEnv,
-checkEnvValue,
-listEnvValues,
-checkFile,
-checkWinrar,
-winrarExtract,
-setEnv,
-listFolder,
-mkDir,
-copyFile
-*/
